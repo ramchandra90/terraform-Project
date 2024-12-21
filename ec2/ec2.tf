bcc0bb42e7b4569917f94bd884821b8064ef25fe
ramachandra <ramchandra081@gmail.com>
2024-12-22 12:32:52 +1300

    functions

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
 }
 resource "aws_instance" "terraform" {
   ami = "ami-09c813fb71547fc4f"
-  instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"
+  instance_type = "t3.micro"
    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
   tags = {
     Name = "terraform"
