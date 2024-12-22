df7712d29f42cd3eaa71c3b6fe869b6d130d3ffb
ramachandra <ramchandra081@gmail.com>
2024-12-22 15:47:35 +1300

    functions

--- count/ec2.tf
+++ count/ec2.tf
@@ -28,6 +28,6 @@ resource "aws_instance" "backend" {
   instance_type = "t3.micro"
    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
   tags = {
-    Name = "backend"
+    Name = var.instance_names[count.index]
   }
 }
